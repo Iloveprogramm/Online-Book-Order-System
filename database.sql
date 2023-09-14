24eaeac7611271c8eb422ebf3a666d0a7f1983df
Mandana Ebrahimian <75654442+manderoon@users.noreply.github.com>
2023-09-14 22:22:53 +1000

    Renamed SQL

---
+++ database.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Books (
+    BookID INT AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(255) NOT NULL,
+    Author VARCHAR(255) NOT NULL,
+    Price DECIMAL(10, 2),
+    ImageURL VARCHAR(255),
+    Category VARCHAR(255) NOT NULL
+);
+
+INSERT INTO Books (Title, Author, Price, ImageURL, Category) VALUES
+('Thinking in Java', 'Bruce Eckel', 99.00, './Image/Effective Java Programming.png', 'Programming'),
+('The Marxification of Education', 'James Lindsay', 28.48, './Image/The Marxification of Education(Education).png', 'Education'),
+('Reminders of Him', 'Colleen Hoover', 16.00, './Image/Reminders of Him(Novel).png', 'Novel'),
+('The Complete Far Side', 'Gary Larson', 200.00, './Image/The Complete Far Side(Cartoon).png', 'Cartoon'),
+('History of the World Map by Map', 'DK', 70.61, './Image/History of the World Map by Map.png', 'History');
+
+CREATE DATABASE UserDB;
+USE UserDB;
+CREATE TABLE UserTable (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Wishlist (
+    user_id INT,
+    book_id INT,
+    date_added DATE NOT NULL,
+    PRIMARY KEY (user_id, book_id),
+    FOREIGN KEY (user_id) REFERENCES UserTable(id),
+    FOREIGN KEY (book_id) REFERENCES Books(BookID)
+);
+
+CREATE TABLE Reviews (
+    ReviewID INT AUTO_INCREMENT PRIMARY KEY,
+    BookID INT NOT NULL,
+    ReviewerName VARCHAR(255) NOT NULL,
+    Rating INT NOT NULL,
+    ReviewText TEXT,
+    ReviewDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (BookID) REFERENCES Books(BookID)
+);
+
+INSERT INTO Reviews (BookID, ReviewerName, Rating, ReviewText) VALUES
+(1, 'John Doe', 4, 'Great book! I loved the characters and the plot.'),
+(2, 'Jane Smith', 5, 'An absolute masterpiece. This book is a must-read for everyone.'),
+(3, 'Alice Johnson', 3, 'The book was okay, but it could have been better.'),
+(4, 'Bob Wilson', 4, 'Enjoyed reading it. The author did a great job.'),
+(5, 'Eva Brown', 5, 'One of the best books I have ever read. Highly recommended.');
