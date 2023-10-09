43cf0dafb9ce553bc2d80bb148a1626c8cdbf47b
Mandana Ebrahimian <75654442+manderoon@users.noreply.github.com>
2023-10-09 19:22:03 +1100

    Feature Issue #2 - Review can be added to DB Need to redirect user to new page

--- addReviewProcess.php
+++ addReviewProcess.php
@@ -1,27 +1,37 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bookonlineorder";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+function addReviewToDatabase($data)
+{
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "bookonlineorder";
 
-// Check connection
-if ($conn->connect_error) {
-    die(json_encode(['status' => 'error', 'message' => "Connection failed: " . $conn->connect_error]));
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        return json_encode(['status' => 'error', 'message' => "Connection failed: " . $conn->connect_error]);
+    }
+
+    $stmt = $conn->prepare("INSERT INTO Reviews (BookID, ReviewerName, Rating, ReviewText) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("isss", $bookID, $reviewerName, $rating, $reviewText);
+
+    $bookID = $data['bookID'];
+    $reviewerName = $data['reviewerName'];
+    $rating = $data['rating'];
+    $reviewText = $data['reviewText'];
+
+    if ($stmt->execute()) {
+        $stmt->close();
+        $conn->close();
+        return json_encode(['status' => 'success', 'message' => 'Review added successfully!']);
+    } else {
+        return json_encode(['status' => 'error', 'message' => 'Error: ' . $stmt->error]);
+    }
 }
 
-// Prepare statement to prevent SQL injection
-$stmt = $conn->prepare("INSERT INTO Reviews (BookID, ReviewerName, Rating, ReviewText) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("issi", $bookID, $reviewerName, $rating, $reviewText);
-
-// Grab the POST data from the review form submission
-$bookID = $_POST['bookID']; // TODO: Need to get book ID from title instead of using ID
-$reviewerName = $_POST['reviewerName'];
-$rating = $_POST['rating'];
-$reviewText = $_POST['reviewText'];
-
-$stmt->close();
-$conn->close();
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    header('Content-Type: application/json');
+    echo addReviewToDatabase($_POST);
+}
 ?>
