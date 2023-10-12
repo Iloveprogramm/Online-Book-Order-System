76a9399dd640a42935e8afa7b5ff3997cdcbd126
LM_JM <96208001+Iloveprogramm@users.noreply.github.com>
2023-10-12 21:11:46 +1100

    change

--- editSingleBook.php
+++ editSingleBook.php
@@ -10,17 +10,7 @@ if (php_sapi_name() === 'cli') {
 }
 
 function getDatabaseConnection() {
-    $servername = "127.0.0.1";
-$username = "testuser";
-$password = "TestPass123!"; 
-$dbname = "bookonlineorder";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    
-    return $conn;
+    include 'db_config.php';
 }
 
 function editBook($conn, $bookData) {
