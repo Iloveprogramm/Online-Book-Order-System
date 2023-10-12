3401d385b150076633fec8dcdce8c3a9e0baf6df
Mandana Ebrahimian <75654442+manderoon@users.noreply.github.com>
2023-10-13 09:20:41 +1100

    Updated db Conn

--- dbConnection.php
+++ dbConnection.php
@@ -1,19 +1,16 @@
 <?php
-
-if (!function_exists('getConnection')) {
     function getConnection() {
         $servername = "localhost";
-        $username = "username";
-        $password = "password";
-        $dbname = "database";
+        $username = "root";
+        $password = "";
+        $dbname = "bookonlineorder";
 
+        // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
-
+        // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-
         return $conn;
     }
-}
 ?>
