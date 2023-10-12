0b79dcc5219688e819104c2721cfc489feb8c97f
LM_JM <96208001+Iloveprogramm@users.noreply.github.com>
2023-10-12 23:07:53 +1100

    change

--- addToCart.php
+++ addToCart.php
@@ -3,48 +3,31 @@
 function addToCart($bookId, $existingCartId = null, $setCookie = true) {
 
     include 'db_config.php';
+    
+    // If there is already a shopping cart ID, use it, otherwise generate a unique ID as the shopping cart ID
+    $cartId = $existingCartId ?? uniqid(); 
 
-    // Check if the database connection exists and is an object
-    if (!$conn || !is_object($conn)) {
-        return "Error: Unable to connect to the database.";
-    }
-
-   //Verify book ID
-    if (!is_numeric($bookId) || $bookId <= 0) {
-        return "Error: Invalid book ID.";
-    }
-
-    $cartId = $existingCartId ?? uniqid();
-
+    // If $setCookie is true, store the cart ID in a cookie for future requests
     if ($setCookie) {
-        setcookie('cart_id', $cartId, time() + (86400 * 30), "/", "", false, true); // Added httponly parameter
+        setcookie('cart_id', $cartId, time() + (86400 * 30), "/"); 
     }
 
-   // Use prepared statements and parameter binding to prevent SQL injection
-    $stmt = $conn->prepare("INSERT INTO cart (cart_id, book_id, quantity) VALUES (?, ?, 1)
-                            ON DUPLICATE KEY UPDATE quantity = quantity + 1");
+    // Prepare SQL statement. 
+    //If the book is already in the shopping cart, add one to the quantity, otherwise add the book to the shopping cart
+    $sql = "INSERT INTO cart (cart_id, book_id, quantity) VALUES ('$cartId', $bookId, 1)
+            ON DUPLICATE KEY UPDATE quantity = quantity + 1"; 
 
-    if ($stmt) {
-        $stmt->bind_param("si", $cartId, $bookId);
-
-        if ($stmt->execute()) {
-            $stmt->close();  // After using prepared statements, the statement should be closed
-            $conn->close();
-            return "Book added to cart successfully";
-        } else {
-            $error = "Error: " . $stmt->error;
-        }
+    if ($conn->query($sql) === TRUE) {
+        $conn->close();
+        return "Book added to cart successfully";
     } else {
-        $error = "Error: " . $conn->error;
+        $error = "Error: " . $sql . "<br>" . $conn->error;
+        $conn->close();
+        return $error;
     }
-
-    $conn->close();
-    return $error;
 }
 
-// Make sure it's a GET request and the book ID is set
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['book_id'])) {
+// Check whether a GET request has been received, if so, call the addToCart function
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'GET') {
     echo addToCart($_GET['book_id'], $_COOKIE['cart_id'] ?? null);
-} else {
-    echo "Error: Invalid request.";
 }
