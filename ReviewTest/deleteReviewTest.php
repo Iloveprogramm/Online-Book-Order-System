bd84911b5753f8fc72d65edaa4f8a35e41b0874e
Mandana Ebrahimian <75654442+manderoon@users.noreply.github.com>
2023-10-13 22:43:13 +1100

    update review test

--- ReviewTest/deleteReviewTest.php
+++ ReviewTest/deleteReviewTest.php
@@ -1,19 +1,32 @@
 <?php
-require_once ("deleteReview.php");
 
-function testDeleteReview()
+require_once 'deleteReview.php';
+use PHPUnit\Framework\TestCase;
+
+class DeleteReviewTest extends TestCase
 {
-    $data = [
-        'reviewID' => 1
-    ];
+    public function testDeleteReviewSuccess()
+    {
+        // good review id to make sure it runs
+        $reviewID = 1;
 
-    $result = json_decode(deleteReview($data), true);
+        $response = deleteReview($reviewID);
+        $decodedResponse = json_decode($response, true);
 
-    if ($result['status'] === 'success') {
-        echo "Review deleted successfully!\\n";
-    } else {
-        echo "Error deleting review: " . $result['message'] . "\\n";
+        $this->assertEquals('success', $decodedResponse['status']);
+        $this->assertEquals('Review deleted successfully!', $decodedResponse['message']);
+    }
+
+    public function testDeleteReviewFailure()
+    {
+        // review that doesnt exist to see if it fails
+        $reviewID = 999;
+
+        $response = deleteReview($reviewID);
+        $decodedResponse = json_decode($response, true);
+
+        $this->assertEquals('error', $decodedResponse['status']);
+        $this->assertStringContainsString('Error', $decodedResponse['message']);
     }
 }
-
 ?>
