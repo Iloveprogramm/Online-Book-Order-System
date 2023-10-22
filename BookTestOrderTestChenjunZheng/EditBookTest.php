5168aeb7bad344f7b51ee7c5c88aefc010c38c65
LM_JM <96208001+Iloveprogramm@users.noreply.github.com>
2023-10-22 15:13:16 +1100

    test

--- BookTestOrderTestChenjunZheng/EditBookTest.php
+++ BookTestOrderTestChenjunZheng/EditBookTest.php
@@ -1,5 +1,4 @@
 <?php
-//Importing PHPUnit's TestCase class for creating test cases
 use PHPUnit\Framework\TestCase;
 
 require './editSingleBook.php'; 
@@ -10,53 +9,41 @@ class EditBookTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->conn = getDatabaseConnection();
+        $this->conn = getDatabaseConnection(); // Ensure this function is well-defined and error-handled
     }
 
-    // Called after each test method is executed to clean up the test environment
     protected function tearDown(): void
     {
         $this->conn->close();
     }
 
     public function testEditBookOnRealDatabase()
-{
+    {
+        // Fetch initial book details
+        $initialData = getBookDetails($this->conn, 1); // Ensure this function is well-defined and error-handled
 
-    // Get and store book details before testing
-    $initialData = getBookDetails($this->conn, 1);
+        // Prepare data for modification
+        $modifiedData = [
+            'bookID' => 1, 
+            'title' => 'Test Title',
+            'author' => 'Test Author',
+            'price' => 9.99,
+            'category' => 'Test Category'
+        ];
 
+        // Edit the book with the modified data
+        editBook($this->conn, $modifiedData); // Ensure this function is well-defined and error-handled
 
-    $initialData['bookID'] = $initialData['BookID'];  
-    $initialData['title'] = $initialData['Title'];
-    $initialData['author'] = $initialData['Author'];
-    $initialData['price'] = $initialData['Price'];
-    $initialData['category'] = $initialData['Category'];
-    
+        // Fetch the updated book details
+        $updatedData = getBookDetails($this->conn, 1);
 
-    // Define new book details for the simulation
-    $mockBookData = [
-        'bookID' => 1, 
-        'title' => 'Test Title',
-        'author' => 'Test Author',
-        'price' => 9.99,
-        'category' => 'Test Category'
-    ];
-    
+        // Assert the book details were updated correctly
+        $this->assertEquals($modifiedData['title'], $updatedData['Title']);
+        $this->assertEquals($modifiedData['author'], $updatedData['Author']);
+        $this->assertEquals($modifiedData['price'], $updatedData['Price']);
+        $this->assertEquals($modifiedData['category'], $updatedData['Category']);
 
-    //Call the editBook function to edit using simulated new book data
-    editBook($this->conn, $mockBookData);
-
-
-    $updatedBookData = getBookDetails($this->conn, $mockBookData['bookID']);
-
-    // Assert that the edited book details match the simulated new book data
-    $this->assertEquals($mockBookData['title'], $updatedBookData['Title']);
-    $this->assertEquals($mockBookData['author'], $updatedBookData['Author']);
-    $this->assertEquals($mockBookData['price'], $updatedBookData['Price']);
-    $this->assertEquals($mockBookData['category'], $updatedBookData['Category']);
-
-
-    //Restore the original state of the book using the book details before testing
-    editBook($this->conn, $initialData);
-}
+        // Restore the original book details
+        editBook($this->conn, $initialData);
+    }
 }
