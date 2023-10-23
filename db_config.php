c1e359b3517726f127d963ca852f793322eb9731
Chenjun Zheng <Chenjun.Zheng-1@student.uts.edu.au>
2023-10-23 15:27:07 +1100

    change

--- db_config.php
+++ db_config.php
@@ -7,3 +7,11 @@ $dbname = "bookonlineorder";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
+
+<!--
+CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'TestPass123!';
+
+GRANT ALL PRIVILEGES ON bookonlineorder.* TO 'testuser'@'localhost';
+
+FLUSH PRIVILEGES;
+-->
