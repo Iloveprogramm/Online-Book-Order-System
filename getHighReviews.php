a773d7ef276e2415cfa65184f234d36c8656f545
Iloveprogramm <96208001+Iloveprogramm@users.noreply.github.com>
2023-10-23 16:14:38 +1100

    Update getHighReviews.php

--- getHighReviews.php
+++ getHighReviews.php
@@ -1,26 +1,42 @@
 <?php
-include("dbConnection.php");
 
-// function searchHighestRatedBooks() {
-    $conn = getConnection();
+$servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "bookonlineorder";
 
-    $sql = "SELECT Rating FROM Books WHERE Rating >= 4 ORDER BY Rating DESC";
-    $result = $conn->query($sql);
-    
-    if ($result->num_rows > 0) {
-        echo '<div class="container">';
-        echo '<div class="row">';
-        while ($row = $result->fetch_assoc()) {
-            echo '<div class="col">';
-            echo '<p>' . $row["Rating"] . ' Stars</p>';
-            echo '</div>';
-        }
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT * FROM Reviews WHERE Rating = (SELECT MAX(Rating) FROM Reviews) ORDER BY ReviewDate DESC";
+$result = $conn->query($sql);
+
+if ($conn->error) {
+    die("SQL Error: " . $conn->error); 
+}
+
+if ($result->num_rows > 0) {
+    echo '<div class="container">';
+    echo '<div class="row">';
+    while ($row = $result->fetch_assoc()) {
+        echo '<div class="col">';
+        echo '<p>Book ID: ' . $row["BookID"] . '</p>';
+        echo '<p>Reviewer: ' . $row["ReviewerName"] . '</p>';
+        echo '<p>Rating: ' . $row["Rating"] . ' Stars</p>';
+        echo '<p>Review: ' . $row["ReviewText"] . '</p>';
+        echo '<p>Date: ' . $row["ReviewDate"] . '</p>';
         echo '</div>';
-        echo '</div>';
-    } else {
-        echo "<p>No highly rated books found.</p>";
     }
+    echo '</div>';
+    echo '</div>';
+} else {
+    echo "<p>No reviews found.</p>";
+}
 
-    $conn->close();
-// }
+$conn->close();
 ?>
