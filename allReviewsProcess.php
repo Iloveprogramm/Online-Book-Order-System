29bc8501be51a53ac72d38209987516347e1e2f0
Iloveprogramm <96208001+Iloveprogramm@users.noreply.github.com>
2023-10-23 16:15:28 +1100

    Update allReviewsProcess.php

--- allReviewsProcess.php
+++ allReviewsProcess.php
@@ -1,54 +1,26 @@
-<script>
-    function confirmDelete(reviewID) {
-        if (confirm('Are you sure you want to delete this review?')) {
-            // send request to delete review
-            fetch('deleteReview.php', {
-                method: 'POST',
-                body: JSON.stringify({ reviewID: reviewID }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === 'success') {
-                    // refresh after deleted
-                    location.reload()
-                } else {
-                    // check error
-                    alert('Error: ' + data.message);
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                // check error
-                alert('There was an error. Please try again later.');
-            });
-        }
-    }
-</script>
-
 <?php
 include("dbConnection.php");
 
 function displayAllReviews() {
     $conn = getConnection();
 
-    $sql = "SELECT * FROM Reviews";
+    $sql = "SELECT Reviews.*, Books.Title FROM Reviews JOIN Books ON Reviews.BookID = Books.BookID";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        // print review cards
         echo '<div class="container">';
         echo '<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">';
         while ($row = $result->fetch_assoc()) {
             echo '<div class="col">';
             echo '<div class="card shadow-sm">';
             echo '<div class="card-body">';
-            echo '<h5 class="card-title">' . $row["Title"] . '</h5>';
-            echo '<h6 class="card-subtitle mb-2 text-muted">' . $row["ReviewerName"] . '</h6>';
-            echo '<p class="card-text">' . $row["ReviewText"] . '</p>';
-            echo '<p class="card-text">Rating: ' . $row["Rating"] . '</p>';
+            
+
+            echo '<h5 class="card-title">' . htmlspecialchars($row["Title"]) . '</h5>'; 
+            
+            echo '<h6 class="card-subtitle mb-2 text-muted">' . htmlspecialchars($row["ReviewerName"]) . '</h6>';
+            echo '<p class="card-text">' . htmlspecialchars($row["ReviewText"]) . '</p>';
+            echo '<p class="card-text">Rating: ' . htmlspecialchars($row["Rating"]) . '</p>';
             echo '<button class="btn btn-danger" onclick="confirmDelete(' . $row["ReviewID"] . ')">Delete</button>';
             echo '</div>';
             echo '</div>';
@@ -63,5 +35,4 @@ function displayAllReviews() {
     $conn->close();
 }
 ?>
-
 
