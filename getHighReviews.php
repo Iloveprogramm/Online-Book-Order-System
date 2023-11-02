f81b8829cc11fb3af313e8fa6681a00a2605e5e2
Mandana Ebrahimian <75654442+manderoon@users.noreply.github.com>
2023-11-03 08:28:30 +1100

    test

--- getHighReviews.php
+++ getHighReviews.php
@@ -1,11 +1,5 @@
 <?php
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
 include("dbConnection.php");
 
     $conn = getConnection();
